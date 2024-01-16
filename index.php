13adc4534e5256ff54470c5ee33ed22e5b4c5a56
Chris <chris.tasche@mtec-cc.com>
2024-01-16 12:27:03 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$pf =  '.' . DIRECTORY_SEPARATOR . 'parts' . DIRECTORY_SEPARATOR;
+include $pf . '_header.php';
+include $pf . '_navigation.php';
+include $pf . '_pagehead.php';
+
+include $pf . 'mainview' . DIRECTORY_SEPARATOR . 'main.php';
+
+include $pf . '_footer.php';
